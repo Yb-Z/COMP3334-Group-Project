b42681df83495bf7675344b8f22569c4873dc709
toolsmax0 <xingshijichina@outlook.com>
2022-04-17 23:17:03 +0800

    pgp upd

--- marketapp/pgp.py
+++ marketapp/pgp.py
@@ -3,20 +3,22 @@ import pgpy
 def get_pgp_string(s):
     key = pgpy.PGPKey.new(PubKeyAlgorithm.RSAEncryptOrSign, 4096)
 
-    uid = pgpy.PGPUID.new('DAP')
+    uid = pgpy.PGPUID.new('DAP',comment='DAP',email='admin@dap.com')
 
     key.add_uid(uid, usage={KeyFlags.Sign, KeyFlags.EncryptCommunications, KeyFlags.EncryptStorage},
             hashes=[HashAlgorithm.SHA256, HashAlgorithm.SHA384, HashAlgorithm.SHA512, HashAlgorithm.SHA224],
             ciphers=[SymmetricKeyAlgorithm.AES256, SymmetricKeyAlgorithm.AES192, SymmetricKeyAlgorithm.AES128],
             compression=[CompressionAlgorithm.ZLIB, CompressionAlgorithm.BZ2, CompressionAlgorithm.ZIP, CompressionAlgorithm.Uncompressed])
 
-    #message = pgpy.PGPMessage.new(s)
+    # message = pgpy.PGPMessage.new(s)
 
+    m = "-----BEGIN PGP SIGNED MESSAGE-----\nHash: SHA256\n\n"
+    m+=s
+    m+='\n'
     message = key.sign(s)
+    m+=str(message)
 
-    print(str(message))
-
-    return str(message)
+    return m
 
 
 
